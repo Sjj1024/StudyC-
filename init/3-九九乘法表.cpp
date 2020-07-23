57d28c33b971205bc6baffbd81235e262b01b4bd
1024小神 <48399687+Sjj1024@users.noreply.github.com>
2020-07-23 11:44:52 +0800

    Create 3-九九乘法表.cpp

---
+++ init/3-九九乘法表.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "输出九九乘法表\n";
+	for (int i = 1; i <= 9; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << j << "*" << i << "=" << j * i << "\t";
+		}
+		cout << "\n";
+	}
+}
